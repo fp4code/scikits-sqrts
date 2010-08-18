f41f0a7c914a42714a0370a61afed3f869e3ad4e
Fabrice Pardo <fp4code@gmail.com>
2010-08-18 12:24:26 +0200

    Added doctest strings

--- scikits/sqrts.py
+++ scikits/sqrts.py
@@ -3,22 +3,55 @@
 import numpy as np
 
 def nmv(z):
-    """Nevière-Maystre-Vincent square-root.
-    Cf. http://dx.doi.org/10.1088/0150-536X/8/4/002"""
-    return np.sqrt(1j)*np.sqrt(-1j*z)
+    """Nevière-Maystre-Vincent complex square-root.
+    Cf. http://dx.doi.org/10.1088/0150-536X/8/4/002
+
+    >>> sqrts.nmv([-2j-1e-20,-2j,-2j+1e-20])
+    array([ 1.+1.j,  1.-1.j, -1.-1.j])
+    """
+    return np.sqrt(1j)*np.sqrt(-1j*np.asarray(z,np.complex))
 
 def nmvo(z):
-    """Nevière-Maystre-Vincent square-root, opposite branch cut"""
-    return -np.sqrt(-1j)*np.sqrt(1j*z)
+    """Nevière-Maystre-Vincent complex square-root, opposite branch cut
+
+    >>> sqrts.nmvo([2j+1e-20,2j,2j-1e-20])
+    array([-1.-1.j, -1.-1.j,  1.+1.j])
+    """
+    return -np.sqrt(-1j)*np.sqrt(1j*np.asarray(z,np.complex))
 
 def posimag(z):
-    """Positive imaginary part"""
+    """Positive imaginary part complex square root
+
+    >>> sqrts.posimag([1+1e-20j,1,1-1e-20j]).real
+    array([ 1.,  1., -1.])
+    """
     return 1j*np.conj(np.sqrt(-np.conj(z)))
 
 class Nearest:
-    """nearest square root"""
+    """Nearest complex square root
+    >>> s = sqrts.Nearest([1,1j,-1,-1j])
+    >>> s.sqrt(1).real
+    array([ 1.,  1., -1.,  1.])
+    >>> s = sqrts.Nearest(1)
+    >>> s.sqrt(2j)
+    (1+1j)
+    >>> s.sqrt(-1)
+    1j
+    >>> s.sqrt(-2j)
+    (-1+1j)
+    >>> s.sqrt(1)
+    (-1+0j)
+    >>> s.sqrt(2j)
+    (-1-1j)
+    >>> s.sqrt(-1)
+    (-0-1j)
+    >>> s.sqrt(-2j)
+    (1-1j)
+    >>> s.sqrt(1)
+    (1+0j)
+    """
     def __init__(self, value):
-        self.previous = np.complex(value)
+        self.previous = np.asarray(value,np.complex)
     def sqrt(self,z): 
         s = np.sqrt(np.complex(z))
         d1 = np.abs(self.previous - s)
